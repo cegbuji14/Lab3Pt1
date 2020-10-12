a74a2dacdd54b517fdac9d8d849a19c5f15dda7d
cegbuji14 <57194902+cegbuji14@users.noreply.github.com>
2020-10-11 22:13:41 -0400

    Add files via upload

---
+++ lab-3--part-1---processes-and-pipes/pipes_processes3.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+/**
+ * Executes the command "cat scores | grep Lakers".  In this quick-and-dirty
+ * implementation the parent doesn't wait for the child to finish and
+ * so the command prompt may reappear before the child terminates.
+ *
+ */
+
+int randNumGen(){
+  srand((time(NULL)*390));
+  int randNum = ((rand()%1)+1);//no more than 30 loops
+  return randNum;
+}
+
+int main(int argc, char **argv)
+{
+  int pipefd[2];
+  int pid, pid2;
+
+  char *cat_args[] = {"cat", "scores", NULL};
+  char *grep_args[] = {"grep", "-i", argv[0], "scores", NULL};
+  char *sort_args[] = {"sort", "scores", NULL};
+
+  // make a pipe (fds go in pipefd[0] and pipefd[1])
+
+  pipe(pipefd);
+
+  pid = fork();
+
+  if (pid == 0)
+    {
+      // child gets here and handles "grep Villanova"
+
+      // replace standard input with input part of pipe
+
+      dup2(pipefd[0], 0);
+
+      // close unused hald of pipe
+
+      close(pipefd[1]);
+
+      // execute grep
+
+      execvp("grep", grep_args);
+    }
+  else
+    {
+      pid2 = fork();
+      if (pid2 == 0){
+        
+        //executes sort
+        sleep(randNumGen());
+        dup2(pipefd[1], 1);//prolly wrong just trying stuff
+        
+        close(pipefd[0]);
+        execvp("sort", sort_args);
+      }
+    
+      // parent gets here and handles "cat scores"
+
+      // replace standard output with output part of pipe
+
+      dup2(pipefd[1], 1);
+
+      // close unused unput half of pipe
+
+      close(pipefd[0]);
+
+      // execute cat
+
+     // execvp("cat", cat_args);
+    }
+}
